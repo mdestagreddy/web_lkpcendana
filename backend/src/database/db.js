5d835f256697075c6e0c9c332c265195840541f6
mdestagreddy <gsyv6645@gmail.com>
2026-08-14 13:48:47 +0800

    fix error database from db,js

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,18 +1,25 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const connectionPool = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
+const connectionPool = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost', 
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || 'root',
     database: process.env.DB_SCHEME || 'db_server',
-    multipleStatements: true
+    multipleStatements: true,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-connectionPool.connect(err => {
-    if (err) throw err;
-    console.log("Berhasil menghubungkan MySQL");
+connectionPool.getConnection((err, connection) => {
+    if (err) {
+        console.error("Gagal menghubungkan MySQL:", err.message);
+        throw err;
+    }
+    console.log("Berhasil menghubungkan MySQL melalui Connection Pool!");
+    connection.release();
 });
 
 module.exports = connectionPool;
